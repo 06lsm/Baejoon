1c4976e30ccee9edcaf141a54e2d7e3c0c428730
Lee Simyung <06lsm@naver.com>
2024-08-20 21:36:45 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 60 ms, Memory: 15616 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-
 int board[1001][1001];
 int vis[1001][1001][2];
 int dx[4] = { 1,0,-1,0 };
@@ -18,6 +15,7 @@ int main(void) {
     string input;
     cin >> N >> M;
 
+    //board 배열 입력받기
     for (int i = 0; i < N; i++) {
         cin >> input;
         for (int j = 0; j < M; j++) {
@@ -25,14 +23,18 @@ int main(void) {
         }
     }
 
+    //맵이 한칸인 경우 연산할거 없이 바로 종료
     if (N == 1 && M == 1) {
         cout << 1;
         return 0;
     }
 
+    //bfs 시작할 큐 넣어주기
     que.push({ 0,0,0 });
     vis[0][0][0] = 1;
     vis[0][0][1] = 1;
+
+    //bfs 진행
     while (!que.empty()) {
         auto cur = que.front(); que.pop();
         for (int i = 0; i < 4; i++) {
@@ -40,17 +42,20 @@ int main(void) {
             int ny = get<1>(cur) + dy[i];
             if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
             
+            //case 1. 빈칸인 경우 
             if (board[nx][ny] == 0 && vis[nx][ny][get<2>(cur)] == 0) {
                 vis[nx][ny][get<2>(cur)] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
                 que.push({ nx, ny, get<2>(cur) });
             }
 
+            //case 2. 벽을 부숴야 하는 경우
             if (board[nx][ny] == 1 && get<2>(cur) == 0 && vis[nx][ny][1] == 0) {
                 vis[nx][ny][1] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
                 que.push({ nx,ny,1 });
             }
         }
     }
+    //벽을 부순경우와 안 부수고 간 두가지 경우 중 적게 간 최소거리를 출력
     int ans = 0;
     if (min(vis[N - 1][M - 1][0], vis[N - 1][M - 1][1]) != 0) {
         ans = min(vis[N - 1][M - 1][0], vis[N - 1][M - 1][1]);
